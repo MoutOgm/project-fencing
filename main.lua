58510cb3b285f932d32439fa93153707e40738c1
MoutOgm <philnat29@orange.fr>
2016-07-27 10:48:11 +0200

    epee pour mieu jouer

--- main.lua
+++ main.lua
@@ -11,15 +11,15 @@ function loadPhysics()
   objects.player.body = love.physics.newBody(world, test:getWidth(), test:getHeight(), "dynamic")
   objects.player.shape = love.physics.newRectangleShape(test:getWidth(), test:getHeight())
   objects.player.fixture = love.physics.newFixture(objects.player.body, objects.player.shape)
-  objects.player.fixture:setFriction(0.3)
+  objects.player.fixture:setFriction(0.2)
   objects.player.body:setMass(0.62)
 
   objects.arme = {}
   objects.arme.body = love.physics.newBody(world, 250, 100, "dynamic")
   objects.arme.shape = love.physics.newRectangleShape(epee:getWidth()*3, epee:getHeight()*2)
   objects.arme.fixture = love.physics.newFixture(objects.arme.body, objects.arme.shape)
-  objects.arme.fixture:setFriction(0.75)
-  objects.arme.body:setMass(0.25)
+  objects.arme.fixture:setFriction(0.5)
+  objects.arme.body:setMass(0.3)
 
   objects.ground = {}
   objects.ground.body = love.physics.newBody(world, width/2, height - 300/2)
@@ -93,7 +93,6 @@ function love.draw()
   end
   love.graphics.setColor(252, 45, 201)
   love.graphics.draw(test, enemy.x, enemy.y, 0, 2, 2, test:getWidth()/2, test:getHeight()/2)
-  love.graphics.draw(epee, enemyarme.x, enemyarme.y, 0, 2, 2, test:getWidth()/2, test:getHeight(256)/2)
   love.graphics.setColor(255, 255, 255)
   if isPlaying then
     love.graphics.setColor(255,3,59)
@@ -152,10 +151,14 @@ function move(dt)
   if love.keyboard.isDown("d") then
     objects.player.body:applyForce(speed, 0)
     player.direction = "right"
+    objects.arme.body:applyForce(speed, 0)
+    arme.direction = "right"
   end
   if love.keyboard.isDown("q") then
     objects.player.body:applyForce(-speed, 0)
     player.direction = "left"
+    objects.arme.body:applyForce(-speed, 0)
+    arme.direction = "left"
   end
 end
 -- function move(dt)
